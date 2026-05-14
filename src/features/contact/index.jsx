d95f2116b81519c9ee4da71284b6211ce55c1617
Ashok Kumar <ashokkhichi010@gmail.com>
2026-05-14 19:33:31 +0530

    Refact: contact data updated.

--- src/features/contact/index.jsx
+++ src/features/contact/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Mail, MapPin, Phone, Send } from 'lucide-react'
+import { Mail, Phone, Send } from 'lucide-react'
 import Section from '../../components/Section'
 import './styles.css'
 import githubIcon from '../../assets/icons/github.svg'
 import linkedinIcon from '../../assets/icons/linkedin.svg'
-import xIcon from '../../assets/icons/x.svg'
+import whatsappIcon from '../../assets/icons/whatsapp.svg'
 import contactData from '../../data/contact.json'
 
 const ContactSection = () => {
@@ -12,12 +12,12 @@ const ContactSection = () => {
     <Section id="contact" number="06" title="Contact">
       <div className="contact-wrapper">
         <div className="contact-inner">
-          
+
           <div className="contact-form-container">
             <div className="contact-field">
               <h3 className="contact-title">{contactData.heading}</h3>
               <p className="contact-subtitle">{contactData.subtitle}</p>
-              
+
               <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
                 <input type="text" className="form-input" placeholder="Name" />
                 <input type="email" className="form-input" placeholder="Email" />
@@ -28,12 +28,13 @@ const ContactSection = () => {
               </form>
             </div>
           </div>
-          
+
           <div className="contact-social-bar">
             <ul className="social-list">
-              <li><a href={contactData.socials.find(s => s.platform === 'github')?.url ?? '#'}><img src={githubIcon} alt="Github" width={20} className="social-icon-img" /></a></li>
-              <li><a href={contactData.socials.find(s => s.platform === 'x')?.url ?? '#'}><img src={xIcon} alt="X (Twitter)" width={20} className="social-icon-img" /></a></li>
-              <li><a href={contactData.socials.find(s => s.platform === 'linkedin')?.url ?? '#'}><img src={linkedinIcon} alt="LinkedIn" width={20} className="social-icon-img" /></a></li>
+              <li><a target='_blank' href={contactData.socials.find(s => s.platform === 'github')?.url ?? '#'}><img src={githubIcon} alt="Github" width={20} className="social-icon-img" /></a></li>
+              <li><a target='_blank' href={contactData.socials.find(s => s.platform === 'github2')?.url ?? '#'}><img src={githubIcon} alt="Github" width={20} className="social-icon-img" /></a></li>
+              <li><a target='_blank' href={contactData.socials.find(s => s.platform === 'linkedin')?.url ?? '#'}><img src={linkedinIcon} alt="LinkedIn" width={20} className="social-icon-img" /></a></li>
+              <li><a target='_blank' href={contactData.socials.find(s => s.platform === 'whatsapp')?.url ?? '#'}><img src={whatsappIcon} alt="LinkedIn" width={20} className="social-icon-img" /></a></li>
             </ul>
           </div>
 
@@ -46,10 +47,6 @@ const ContactSection = () => {
             <div className="info-single">
               <Mail className="info-icon" size={20} />
               <span>{contactData.email}</span>
-            </div>
-            <div className="info-single">
-              <MapPin className="info-icon" size={20} />
-              <span>{contactData.location}</span>
             </div>
           </div>
 
